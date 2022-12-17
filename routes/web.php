4f1245cd8663dd9edc723f042279dca57335f53f
imotas228 <tsatomi0228@icloud.com>
2022-12-17 20:01:49 +0900

    コントローラーとモデルの作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PerfumepostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/posts', [PerfumepostController::class, 'index']);
+
